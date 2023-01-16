0e0fab509fddbd5d2eb73789e085d733c709b482
viktor900221 <viktor.legradi@gmail.com>
2023-01-16 10:56:38 +0100

    Babel modifiziert

--- terminkalender/babel.config.js
+++ terminkalender/babel.config.js
@@ -1,5 +1,16 @@
-module.exports = {
+/*module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ]
+}
+*/
+
+module.exports = {
+  presets: [
+    ['@babel/preset-env',
+      {
+        corejs: "core-js@3,"
+      }
+    ]
   ]
 }
--- terminkalender/src/App.vue
+++ terminkalender/src/App.vue
@@ -16,6 +16,7 @@ export default {
 
 <style>
 @import "~bootstrap/dist/css/bootstrap.min.css";
+@import "~fortawesome/fontawesome-free/css/all.min.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
